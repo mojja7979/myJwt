b6ebbc0e5ed38adcd9bcca8585d9356b35624dc1
mjhan21 <mjhan@doctorringo.com>
2022-05-18 11:15:23 +0900

    jwt start

---
+++ myJwt/src/main/java/com/mojja/myjwt/MyJwtApplication.java
@@ -0,0 +1,13 @@
+package com.mojja.myjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyJwtApplication.class, args);
+    }
+
+}
---
+++ myJwt/src/test/java/com/mojja/myjwt/MyJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mojja.myjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
